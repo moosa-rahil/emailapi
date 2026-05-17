24a8aef54b9a5a81c2219486691ae335312c9453
moosa-rahil <iu05-0126-0003@iqra.edu.pk>
2026-05-17 23:45:08 +0500

    Add files via upload
    Cors changes

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // allow all origins
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  }),
+);
+
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
